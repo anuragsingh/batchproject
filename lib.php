fff96ab8511ca5ea299e932b3c7f726b773a4ffd
unknown <lenovo@lenovo-PC.(none)>
2013-04-09 23:28:46 +0530

    show all profile fields

--- lib.php
+++ lib.php
@@ -3,7 +3,21 @@
 require_once('config.php');
 
 function get_all_profile_fields_with_display_order(){
-    $fields_order = array('batchno','batchidno','username','posting','Caste','religion','contact','father','permanent','district','state','pincode');
+    //$fields_order = array('batchno','batchidno','username','posting','Caste','religion','contact','father','permanent','district','state','pincode');
+    $fields_order = array();
+    $sql = 'SELECT uif.shortname FROM '.PREFIX.'user_info_field AS uif ORDER BY uif.sortorder';
+    $result = mysql_query($sql);
+    while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+        $fields_order[] = $row['shortname'];
+    }
+    for($i=count($fields_order)-1;$i>=0;$i--){
+        if($i == 2){
+            $fields_order[2]='username';
+            break;
+        }else{
+            $fields_order[$i+1]=$fields_order[$i];
+        }
+    }
     $fields_display = array();
     $fields_display1 = array();
     $sql = 'SELECT uif.id, uif.shortname, uif.name, uif.description  FROM '.PREFIX.'user_info_field AS uif';
@@ -30,6 +44,6 @@ function get_field_name($f,$k){
         
     switch ($k){
         case 'username': return "NURSE'S NAME";
-        default : return strip_tags($f[$k]['description']); 
+        default : return strip_tags($f[$k]['name']); 
     }
 }
