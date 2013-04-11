7b4b9bece3af839d2b421a883e7c30e0e805692e
unknown <lenovo@lenovo-PC.(none)>
2013-04-11 06:25:40 +0530

    attendance partially

--- lib.php
+++ lib.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once('config.php');
+define('COURSE_CONTEXT',50);
 
 function get_all_profile_fields_with_display_order(){
     //$fields_order = array('batchno','batchidno','username','posting','Caste','religion','contact','father','permanent','district','state','pincode');
@@ -46,4 +47,91 @@ function get_field_name($f,$k){
         case 'username': return "NURSE'S NAME";
         default : return strip_tags($f[$k]['name']); 
     }
+}
+
+
+function get_all_users_course_assigned($users){
+    $courses = array();
+    
+    if($users){
+        $sql = "SELECT e.courseid FROM ".PREFIX."enrol AS e INNER JOIN ".PREFIX."user_enrolments AS ue ON ue.enrolid=e.id 
+INNER JOIN ".PREFIX."context AS c ON c.instanceid=e.courseid
+INNER JOIN ".PREFIX."role_assignments AS ra ON ra.contextid=c.id
+INNER JOIN ".PREFIX."role AS r ON r.id=ra.roleid
+WHERE r.shortname='student' AND c.contextlevel=".COURSE_CONTEXT." AND ue.userid IN ( ".implode(',',$users)." ) GROUP BY e.courseid";
+        
+        $result = mysql_query($sql);
+        while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {            
+            $courses[] = $row['courseid'];
+        }
+    }
+    
+    return $courses;
+}
+
+
+function get_attendance_header_content($courses){
+    
+    $result_return = array();
+    
+    $result_return['head'] = array();
+    $result_return['content'] = array();
+    
+    if($courses) {
+    
+    $sql = "SELECT mal.studentid, mal.statusid, mast.grade, mas.sessdate,mas.attendanceid, ma.course, mas.lasttaken 
+FROM mdl_attendance_log AS mal INNER JOIN mdl_attendance_sessions AS mas ON mas.id = mal.sessionid 
+INNER JOIN mdl_attendance_statuses AS mast ON mast.id = mal.statusid 
+INNER JOIN mdl_attforblock AS ma ON ma.id = mas.attendanceid 
+WHERE ma.course IN ( ".implode(',',$courses)." )  
+GROUP BY mas.sessdate,mas.attendanceid 
+ORDER BY mas.attendanceid ASC, mas.sessdate ASC";
+    
+    $result = mysql_query($sql);
+    while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+        
+        $record = new stdClass();
+        $record->sessdate = $row['sessdate'];
+        $record->attendanceid = $row['attendanceid'];
+        $record->course = $row['course'];
+        
+        $result_return['head'][] = $record;
+    }
+    
+    
+    $sql = "SELECT mal.studentid, mal.statusid, mast.grade, mast.acronym, mas.sessdate,mas.attendanceid, ma.course, mas.lasttaken 
+FROM mdl_attendance_log AS mal INNER JOIN mdl_attendance_sessions AS mas ON mas.id = mal.sessionid 
+INNER JOIN mdl_attendance_statuses AS mast ON mast.id = mal.statusid 
+INNER JOIN mdl_attforblock AS ma ON ma.id = mas.attendanceid 
+WHERE ma.course IN ( ".implode(',',$courses)." ) 
+ORDER BY mas.attendanceid ASC, mas.sessdate ASC";
+    
+    $result = mysql_query($sql);
+    while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+        
+        $record = new stdClass();
+        $record->studentid = $row['studentid'];
+        $record->statusid = $row['statusid'];
+        $record->grade = $row['grade'];
+        $record->acronym = $row['acronym'];
+        $record->sessdate = $row['sessdate'];
+        $record->attendanceid = $row['attendanceid'];
+        $record->course = $row['course'];
+        $record->lasttaken = $row['lasttaken'];
+        
+        if(!isset($result_return['content'][$record->studentid])){
+            $result_return['content'][$record->studentid] = array();
+        }
+        
+        if(!isset($result_return['content'][$record->studentid][$record->attendanceid])){
+            $result_return['content'][$record->studentid][$record->attendanceid] = array();
+        }
+        
+        $result_return['content'][$record->studentid][$record->attendanceid][] = $record;
+        
+    }
+        
+    }
+    
+    return $result_return;
 }
--- report.php
+++ report.php
@@ -7,6 +7,7 @@ require_once('config.php');
 require_once('lib.php');
 
 define('BATCHFIELDNAME','batchno');
+$days_number = array('MON'=>2,'Tue'=>3,'Wed'=>4,'Thu'=>5,'Fri'=>6,'Sat'=>7,'Sun'=>1);
 
 if(isset($_GET['batchno'])){
     $batchnoselected = $_GET['batchno'];
@@ -38,14 +39,17 @@ echo '<div class="selector-main">';
     echo '</div>';    
 echo '</div>';
 
+$user_ids = array();
+
 if($batchnoselected != '' && $batchnoselected != 'selectbatch'){
     $users = array();
     $sql = "SELECT u.id,u.username,u.firstname,u.lastname,u.email,u.city,u.country FROM ".PREFIX."user AS u INNER JOIN ".PREFIX.'user_info_data AS uid ON uid.userid=u.id INNER JOIN '.PREFIX.'user_info_field AS uif ON uif.id=uid.fieldid WHERE uid.data="'.$batchnoselected.'" AND uif.shortname="'.BATCHFIELDNAME.'"';
     $result = mysql_query($sql);
     while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
         $users[$row['id']] = $row;
+        $user_ids[] = $row['id'];
     }
-        
+        //get_all_users_course_assigned($user_ids);die;
 //    echo '<pre>';var_dump(get_all_profile_fields_with_display_order());
 //    var_dump($users);
     echo "<div>";
@@ -53,8 +57,83 @@ if($batchnoselected != '' && $batchnoselected != 'selectbatch'){
     echo "</div>";
     
     $fields = get_all_profile_fields_with_display_order();
+    $attendance_result = get_attendance_header_content(get_all_users_course_assigned($user_ids));//var_dump($attendance_result);die;
+    
     echo "<div class='table-container'>";
     echo "<table>";
+        
+        echo "<tr>";
+            for($i=1;$i<=count($fields);$i++){
+                echo "<th></th>";
+            }
+            if($attendance_result['head']){
+            $previous_attandance = $attendance_result['head'][0]->attendanceid;
+            $counter = 1;
+            foreach($attendance_result['head'] as $headr){                
+                if($previous_attandance == $headr->attendanceid){
+                    echo "<th>";echo $days_number[date('D', $headr->sessdate)];echo "</th>";
+                }else{
+                    echo "<th></th>";echo "<th></th>";
+                    echo "<th>";echo $days_number[date('D', $headr->sessdate)];echo "</th>";
+                }                
+                if($counter == count($attendance_result['head'])){
+                    echo "<th></th>";echo "<th></th>";
+                }
+                $previous_attandance = $headr->attendanceid;
+                $counter++;
+            }
+            }
+            
+        echo "</tr>";
+        
+        echo "<tr>";
+            for($i=1;$i<=count($fields);$i++){
+                echo "<th></th>";
+            }
+            if($attendance_result['head']){
+            $previous_attandance = $attendance_result['head'][0]->attendanceid;
+            $counter = 1;
+            foreach($attendance_result['head'] as $headr){                
+                if($previous_attandance == $headr->attendanceid){
+                    echo "<th>";echo date('D', $headr->sessdate);echo "</th>";
+                }else{
+                    echo "<th></th>";echo "<th></th>";
+                    echo "<th>";echo date('D', $headr->sessdate);echo "</th>";
+                }                
+                if($counter == count($attendance_result['head'])){
+                    echo "<th></th>";echo "<th></th>";
+                }
+                $previous_attandance = $headr->attendanceid;
+                $counter++;
+            }
+            }
+            
+        echo "</tr>";
+        
+        echo "<tr>";
+            for($i=1;$i<=count($fields);$i++){
+                echo "<th></th>";
+            }
+            if($attendance_result['head']){
+            $previous_attandance = $attendance_result['head'][0]->attendanceid;
+            $counter = 1;
+            foreach($attendance_result['head'] as $headr){                
+                if($previous_attandance == $headr->attendanceid){
+                    echo "<th>";echo date('j-M-y', $headr->sessdate);echo "</th>";
+                }else{
+                    echo "<th></th>";echo "<th></th>";
+                    echo "<th>";echo date('j-M-y', $headr->sessdate);echo "</th>";
+                }                
+                if($counter == count($attendance_result['head'])){
+                    echo "<th></th>";echo "<th></th>";
+                }
+                $previous_attandance = $headr->attendanceid;
+                $counter++;
+            }
+            }
+            
+        echo "</tr>";
+        
         echo "<tr>";
             foreach($fields as $fk => $f){
                 echo "<th>";
@@ -62,6 +141,30 @@ if($batchnoselected != '' && $batchnoselected != 'selectbatch'){
                     echo $fname; 
                 echo "</th>";
             }
+            
+            if($attendance_result['head']){
+            $previous_attandance = $attendance_result['head'][0]->attendanceid;
+            $counter = 1;
+            foreach($attendance_result['head'] as $headr){                
+                if($previous_attandance == $headr->attendanceid){
+                    echo "<th>";echo 'Days '.$counter;echo "</th>";
+                }else{
+                    echo "<th>Total Attendance</th>";echo "<th>Attendance (%)</th>";$counter = 1;
+                    echo "<th>";echo 'Days '.$counter;echo "</th>";
+                    $previous_attandance = $headr->attendanceid;
+                    $counter++;
+                    //continue;
+                }     
+                echo $counter;echo count($attendance_result['head']);
+                if($counter == count($attendance_result['head'])){
+                    echo "<th>Total Attendance</th>";echo "<th>Attendance (%)</th>";$counter = 1;
+                }
+                $previous_attandance = $headr->attendanceid;
+                $counter++;
+            }
+            }
+        
+            
         echo "</tr>";
         foreach($users as $uk => $user){
             $sql = "SELECT * FROM ".PREFIX."user_info_data WHERE userid=".$user['id'];
@@ -89,6 +192,54 @@ if($batchnoselected != '' && $batchnoselected != 'selectbatch'){
                         }
                     echo "</td>";
                 }
+                
+                if($attendance_result['head']){
+                $previous_attandance = $attendance_result['head'][0]->attendanceid;
+                $counter = 1;$user_attendance = 0;$is_user_assigned = 0;$total_counter = 1;
+                if(isset($attendance_result['content'][$user['id']])){
+                    $is_user_assigned = 1;
+                }
+                foreach($attendance_result['head'] as $headr){                
+                    if($previous_attandance == $headr->attendanceid){
+                        echo "<td>";
+                        if($is_user_assigned){
+                            if($attendance_result['content'][$user['id']][$headr->attendanceid][$counter-1]->acronym == 'P'){
+                                echo '1';$user_attendance++;
+                            }
+                        }
+                        echo "</td>";
+                    }else{
+                        if($is_user_assigned){
+                            echo "<td>";echo $user_attendance; echo"</td>";
+                            echo "<td>";echo round(($user_attendance/$counter)*100,2).'%'; echo"</td>";
+                            echo "<td>";
+                            $user_attendance = 0;$counter = 1;
+                            if($is_user_assigned){
+                                if($attendance_result['content'][$user['id']][$headr->attendanceid][$counter-1]->acronym == 'P'){
+                                    echo '1';$user_attendance++;
+                                }
+                            }
+                            echo "</td>";
+                        }else{
+                            echo "<td></td>";echo "<td></td>";
+                            $user_attendance = 0;$counter = 1;
+                        }
+                        //continue;
+                    }                
+                    if($total_counter == count($attendance_result['head'])){
+                        if($is_user_assigned){
+                            echo "<td>";echo $user_attendance; echo"</td>";
+                            echo "<td>";echo round(($user_attendance/$counter)*100,2).'%'; echo"</td>";
+                        }else{
+                            echo "<td></td>";echo "<td></td>";
+                        }
+                        $user_attendance = 0;$counter = 1;//continue;
+                    }
+                    $previous_attandance = $headr->attendanceid;
+                    $counter++;$total_counter++;
+                }
+                }
+                
             echo "</tr>";
         }
     echo "</table>";
